a63d9a1356c7dccb39c0336137a9955183a27f62
gentee <akrivonogov@gmail.com>
2021-12-04 17:49:26 +0500

    version 1.24

--- const.go
+++ const.go
@@ -6,7 +6,7 @@ package main
 
 const (
 	// Version of the application
-	Version = "1.23.0"
+	Version = "1.24.0"
 	// DefPort is the default web-server port
 	DefPort = 3234
 	// DefTheme is the default web-server theme
